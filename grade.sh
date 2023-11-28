300112eb0284b71e1a56a9f39fc4091d5759f993
guiuiy <bblu2981@gmail.com>
2023-11-28 12:40:50 -0800

    updated grad.sh

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,46 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+# check student code is correct file submitted
+submission=`find student-submission -name "*.java"`
+echo $submission
+testFile=`find TestListExamples.java`
+# echo $submission
+if [[ ! -f $submission ]] || [[ ! $submission == **/ListExamples.java* ]]
+then 
+    echo "Wrong file or file name submitted"
+    exit
+fi
+
+# get the student code and and move into grading-area
+cp $submission grading-area/ListExamples.java
+cp $testFile grading-area
+cp -r lib grading-area
+
+#run test
+cd grading-area
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java 2> compileError.txt
+java -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" org.junit.runner.JUnitCore TestListExamples > testResult.txt
+
+grep "Failures:" testResult.txt > failureNumber.txt
+failureText=`cat failureNumber.txt`
+failureNumber=`wc -l failureNumber.txt`
+
+if [[ $failureNumber == *1* ]]
+then 
+    readarray -d " " -t num <<< "$failureText"
+    echo "There is at least" ${num[5]} "error:"
+
+    compileErrorNumber=`wc -l compileError.txt`
+
+    if [[ $compileErrorNumber != *0* ]]
+    then
+        echo "Compile error:"
+        cat compileError.txt
+    fi
+
+else
+    echo "100%"
+fi
